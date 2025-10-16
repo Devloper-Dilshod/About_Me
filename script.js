64acae21c101b93884206a30ba33f50a46a4836e
Devloper-Dilshod <sayfiddinovd25@gmail.com>
2025-10-16 18:59:45 +0500

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const chatbotMessages = document.getElementById('chatbotMessages');
 const chatbotInput = document.getElementById('chatbotInput');
 const sendMessage = document.getElementById('sendMessage');
 
-const GEMINI_API_KEY = 'AIzaSyDFDKwoblxAmd7DOwVEQppqvbNehq9QWYo';
+const GEMINI_API_KEY = '';
 
 chatbotToggle.addEventListener('click', () => {
     chatbotWindow.classList.toggle('active');
